6dcc77c99ea401bd4f92ee20f7bd54b9e2e3d297
Bateyjosue <josuebatey19@gmail.com>
2024-06-07 16:36:26 +0200

    feat: display reading list of books

---
+++ frontend/home-test/src/components/ReadingList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { List, ListItem, ListItemText, Button } from '@mui/material';
+
+interface ReadingListProps {
+  books: { title: string; author: string }[];
+  onRemove: (book: { title: string; author: string }) => void;
+}
+
+const ReadingList: React.FC<ReadingListProps> = ({ books, onRemove }) => {
+  return (
+    <List>
+      {books.map((book, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={book.title} secondary={book.author} />
+          <Button variant="contained" color="secondary" onClick={() => onRemove(book)}>
+            Remove
+          </Button>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default ReadingList;
