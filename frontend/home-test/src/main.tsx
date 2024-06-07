b4c8326d5c6695dd0b68597cadf6a03e6c117b22
Bateyjosue <josuebatey19@gmail.com>
2024-06-07 21:48:13 +0200

    feat: add google font Mulish

--- frontend/home-test/src/main.tsx
+++ frontend/home-test/src/main.tsx
@@ -1,13 +1,16 @@
-import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import theme from './theme';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css'
 
-const theme = createTheme({});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline/>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
