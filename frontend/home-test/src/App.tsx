9c7025196dc61867442e75582cf7f0cd515ae168
Bateyjosue <josuebatey19@gmail.com>
2024-06-07 16:19:10 +0200

    feat: setup react and install dependences for material ui

---
+++ frontend/home-test/src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      This Box renders as an HTML section element.
+    </Box>
+  )
+}
+
+export default App
