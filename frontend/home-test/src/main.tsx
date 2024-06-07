2fc44b93ddb445a2a3317748460d2a700f04f345
Bateyjosue <josuebatey19@gmail.com>
2024-06-07 22:56:34 +0200

    feat: add container to wrap the app

--- frontend/home-test/src/main.tsx
+++ frontend/home-test/src/main.tsx
@@ -5,13 +5,16 @@ import theme from './theme';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css'
+import { Container } from '@mui/material';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <App />
+      <CssBaseline />
+      <Container maxWidth="md">
+        <App />
+      </Container>
     </ThemeProvider>
   </React.StrictMode>,
 )
